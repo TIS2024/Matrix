0fb7823ae98720a837c255bf9980febf57eab158
Ayush Salunke <ayush16salunke@gmail.com>
2023-12-18 11:33:33 +0530

    18/12/2023 commit3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/angle_pid/StaticHeading.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/angle_pid/StaticHeading.java
@@ -2,9 +2,12 @@ package org.firstinspires.ftc.teamcode.angle_pid;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.hardware.bosch.BHI260IMU;
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
@@ -23,26 +26,28 @@ public class StaticHeading extends LinearOpMode {
     ElapsedTime timer = new ElapsedTime();
     private double lastError = 0;
 
-    private BNO055IMU imu;
+    private BHI260IMU imu;
 
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         drivetrain.init(hardwareMap);
 
-        imu = hardwareMap.get(BNO055IMU.class, "imu");
-        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.mode = BNO055IMU.SensorMode.IMU;
-        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        imu = hardwareMap.get(BHI260IMU.class, "imu");
+        BHI260IMU.Parameters parameters = imu.getParameters();
         imu.initialize(parameters);
+
+        for (LynxModule module : hardwareMap.getAll(LynxModule.class)) {
+            module.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
+        }
 
         double refrenceAngle = Math.toRadians(90);
         waitForStart();
 
         while(opModeIsActive()){
             telemetry.addData("Target IMU Angle", refrenceAngle);
-            telemetry.addData("Current IMU Angle", imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle);
-            double power = PIDControl(refrenceAngle, imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.RADIANS).firstAngle);
+            telemetry.addData("Current IMU Angle", imu.getRobotOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle);
+            double power = PIDControl(refrenceAngle, imu.getRobotOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.RADIANS).firstAngle);
             drivetrain.power(power);
             drivetrain.moveRobot(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
             telemetry.update();
